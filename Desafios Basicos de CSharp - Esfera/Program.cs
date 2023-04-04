1a15c08115ea483fca8629a963b19fd9f3838145
carlosmg2005 <carlosmg2005@msn.com>
2023-04-04 15:40:42 -0300

    Adicionar arquivos de projeto.

---
+++ Desafios Basicos de CSharp - Esfera/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafios_Basicos_de_CSharp___Esfera
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi, raio, volume;
+            pi = 3.14159;
+            raio = double.Parse(Console.ReadLine());
+            volume = pi * (4.0 / 3) * Math.Pow(raio, 3);
+
+            Console.WriteLine($"VOLUME = {volume:F3}");
+
+            Console.ReadKey();
+        }
+    }
+}
